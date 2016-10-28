a497511a052db5a8dd3d1fc3e9c2ef3c3a193585
zlopi.ru@ <zlopi.ru@gmail.com>
2016-10-28 17:46:53 +0300

    final

--- graid_geli.sh
+++ graid_geli.sh
@@ -1,32 +1,66 @@
 #!/bin/sh
 
-graid delete raid/r0
-graid label intel raid raid1 ada0 ada1
-gpart destroy -F raid/r0
+geli_pass="123"
+raid_disk="ada0 ada1"
+
+graid label intel raid raid1 $raid_disk
 gpart create -s MBR raid/r0
 
 gpart add -t freebsd -s 596m raid/r0
 gpart add -t freebsd raid/r0
 
+gpart set -a active -i1 raid/r0
+
 dd if=/dev/random of=/tmp/r0s2.key count=1 bs=64
-echo "Geli Init"
-geli init -b -K /mnt/boot/r0s2.key /dev/raid/r0s2
-echo "Geli Attach"
-geli attach -k /mnt/boot/r0s2.key /dev/raid/r0s2
+echo $geli_pass > /tmp/pass
+
+geli init -b -J /tmp/pass -K /tmp/r0s2.key /dev/raid/r0s2 || exit
+geli attach -j /tmp/pass -k /tmp/r0s2.key /dev/raid/r0s2 || exit
 
 gpart create -s BSD raid/r0s1
 gpart create -s BSD raid/r0s2.eli
-gpart add -t freebsd-ufs  raid/r0s1
 gpart add -t freebsd-ufs raid/r0s2.eli
 
-newfs /dev/raid/r0s1a
+gpart bootcode -b /boot/mbr raid/r0
+gpart bootcode -b /boot/boot raid/r0s1
+
+glabel label rootfs raid/r0s2.elia
+glabel label bootfs raid/r0s1
+
 newfs /dev/raid/r0s2.elia
 
 mount /dev/raid/r0s2.elia /mnt
-mkdir /mnt/.boot
-mount /dev/raid/r0s1a /mnt/.boot
-mkdir .boot/boot
-cd /mnt && ln -s .boot/boot boot
+cd /mnt
+
+dhclient em0
+fetch http://ftp5.ru.freebsd.org/pub/FreeBSD/releases/amd64/10.3-RELEASE/base.txz
+fetch http://ftp5.ru.freebsd.org/pub/FreeBSD/releases/amd64/10.3-RELEASE/kernel.txz
+tar -xpf base.txz
+tar -xpf kernel.txz
 cp /tmp/r0s2.key /mnt/boot/
+
+newfs /dev/raid/r0s1
+
+mkdir .boot
+mount /dev/raid/r0s1 /mnt/.boot
+mkdir .boot/boot
+
+cp -rp boot .boot
+rm -rf /mnt/boot
+ln -s .boot/boot boot
+
+/bin/cat << EOF > /mnt/boot/loader.conf
+geom_eli_load="YES"
+geom_label_load="YES"
+vfs.root.mountfrom="ufs:label/rootfs"
+geli_raid_keyfile0_load="YES"
+geli_raid_keyfile0_type="raid/r0s2:geli_keyfile0"
+geli_raid_keyfile0_name="/boot/r0s2.key"
+EOF
+
+/bin/cat << EOF > /mnt/etc/fstab
+/dev/label/rootfs	/	ufs	rw	1	1
+/dev/label/bootfs	/.boot	ufs	rw	1	1
+EOF
 
 echo "Done"
